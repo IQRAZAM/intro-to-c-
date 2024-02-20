67fde4c8ba67886709fbe10dc9aa1feef6503e43
Iqra <iqraazam4831@gmail.com>
2024-02-20 19:07:09 +0500

    initialized commit

---
+++ intro.cpp
@@ -0,0 +1,70 @@
+/*#include<iostream>
+using namespace std;
+//its just a simple comment
+   int main(){
+    int star = 2 ;
+     cout<<"hello world!"<< star;
+     return 0;
+   
+                }*/
+
+  /* #include<iostream>
+   using namespace std;
+   int main(){
+      // int a = 4;
+      // int b = 45;
+      int a = 23 , b= 43;
+      float pi = 3.14;
+      char i = 'I';
+      bool var = true;
+      cout<<"this is my first time writing c++ program,tha value of a is "<<a<<  "  and the value of b is "<<b ;
+      cout<<" .\nThe value of pi is: " <<pi;
+      cout<<" .\nThe first letter of my name is " <<i<< " and " <<var;
+
+         return 0;
+   }*/
+   /* #include<iostream>
+    using namespace std;
+
+    int main(){
+      int num1 , num2 ;
+      cout<<"enter the number num1\n";
+      cin>>num1;
+       cout<<"enter the number num2\n";
+      cin>>num2;
+      cout<<"so the sum of these two numbers is " <<num1+num2;
+      return 0;
+
+    }*/
+   //  #include<iostream>
+   //  using namespace std;
+   //   int main(){
+   //    int a=5, b=10;
+      //arthmetic operators 
+      /* cout<<"this is c++ ";
+       cout<<"so the value of a+b is " <<a+b<<endl;
+       cout<<"so the value of a-b is " <<a-b<<endl;
+       cout<<"so the value of a/b is " <<a/b<<endl;
+       cout<<"so the value of a*b is " <<a*b<<endl;
+       cout<<"so the value of a++ is " <<a++<<endl;
+       cout<<"so the value of a-- is " <<a--<<endl;
+       cout<<"so the value of ++a is " <<++a<<endl;
+      cout<<"so the value of --a is " <<--a<<endl;
+      cout<<"so the value of ++a is " <<++a<<endl;
+      cout<<"so the value of a%b is " <<a%b<<endl;*/
+      //comparison operators
+     /* cout<<"the value of a==b is "<<(a==b)<<endl;
+      cout<<"the value of a!=b is "<<(a!=b)<<endl;
+       cout<<"the value of a<=b is "<<(a<=b)<<endl;
+        cout<<"the value of a>=b is "<<(a>=b)<<endl;
+         cout<<"the value of a>b is "<<(a>b)<<endl;
+         cout<<"the value of a<b is "<<(a<b)<<endl;*/
+         //LOGICAL OPERATORS
+   //       cout<<"the value of logical and operator of (a==b) and (a<b) is:  "<<((a==b) && (a<b))<<endl;
+   //       cout<<"the value of logical or operator of (a==b) || (a<b) is: "<<((a==b)||(a<b))<<endl;
+   //        cout<<"the value of logical or operator of (a==b) || (a<b) is: "<<((a==b)||(a<b))<<endl;
+   //     cout<<"the value of logical not operator of  !(a==b)  is: "<<(!(a==b))<<endl;
+      
+      
+   //       return 0;
+   //   }
