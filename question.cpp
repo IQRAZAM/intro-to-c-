f25d53fdeaa6dc34d0b8fe6145839646c1b60fc0
Iqra <iqraazam4831@gmail.com>
2024-03-08 18:58:16 +0500

    added a new file

---
+++ question.cpp
@@ -0,0 +1,49 @@
+//Q1 print a number from 0 to 100
+/*#include<iostream>
+using namespace std;
+int main(){
+  for(int i = 0; i<=100; i++){
+    cout<<i<<endl;
+  }
+return 0;
+}*/
+    //Q2 printing a table 
+    /*#include<iostream>
+    using namespace std;
+    int n;
+    
+    int main(){
+        cout<<"enter the value of n "<<endl;
+        cin>>n;
+       for(int a = 1; a<=10;a++){
+        cout<<n<<" * "<<a<<" = "<<n*a<<endl;
+       }
+    return 0;
+    }*/
+    //Q3 how to make reverse of a table 
+   /* #include<iostream>
+    using namespace std;
+    int main(){
+    int n ;
+    cout<<"enter the valiue of n "<<endl;
+    cin>>n;
+    for(int i = 10; i>=1; i--){
+        cout<<n<<" * "<<i<<" = "<<n*i<<endl;
+    }
+    return 0;
+    }*/
+    //Q4 to check if the number is even or odd 
+ /*#include<iostream>
+ using namespace std;
+ int main(){
+ int n ; 
+ cout<<"enter the value of n"<<endl;
+ cin>>n;
+ if(n%2==0){
+    cout<<"this is an even number"<<endl;
+ }else{
+    cout<<"this is an odd number "<<endl;
+ }
+ return 0;
+ }*/
+ //Q5 
