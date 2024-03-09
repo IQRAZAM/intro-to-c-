464b4c3e664bccb66c62a90943a0b1347e750028
Iqra <iqraazam4831@gmail.com>
2024-03-09 20:46:20 +0500

    modified

--- question.cpp
+++ question.cpp
@@ -46,4 +46,32 @@ return 0;
  }
  return 0;
  }*/
- //Q5 
+ //Q5 print odd or even numbers only from 1 to 100
+/* #include<iostream>
+ using namespace std;
+ int main(){
+ for(int i = 1;i<=100;i+=2){
+    cout<<i<<endl;
+
+ }
+ return 0;
+ }*/
+ /*#include<iostream>
+ using namespace std;
+ int main(){
+ for(int i = 0;i<=100;i+=2){ //even
+    cout<<i<<endl;
+
+ }
+ return 0;
+ }*/
+ // Q6 all tables in one console 
+ #include<iostream>
+ using namespace std;
+ int main(){
+ int a=2,b=3,c=4,d=5,e=6,f=7,g=8,h=9;
+ for(int i= 1;i<=10;i++){
+    cout<<a*i<<b*i<<c*i<<d*i<<e*i<<f*i<<g*i<<h*i<<endl;
+ }
+ return 0;
+ }
